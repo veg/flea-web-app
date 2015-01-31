850fb6dd66be6940246389d9831358c81ee0bec9
Kemal Eren <kemal@kemaleren.com>
2015-01-31 00:15:47 -0800

    got bootstrap working

--- Brocfile.js
+++ Brocfile.js
@@ -18,5 +18,25 @@ var app = new EmberApp();
 // along with the exports of each module as its value.
 
 app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {
+  destDir: 'assets'
+});
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', {
+  destDir: 'fonts'
+});
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', {
+  destDir: 'fonts'
+});
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', {
+  destDir: 'fonts'
+});
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
+  destDir: 'fonts'
+});
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2', {
+  destDir: 'fonts'}
+);
+
+app.import('bower_components/bootstrap/dist/js/bootstrap.js');
 
 module.exports = app.toTree();
