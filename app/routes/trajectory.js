5cd37a752102b32469f949053999664f4a8163e9
Kemal Eren <kemal@kemaleren.com>
2015-01-31 02:52:49 -0800

    cast rates-phylo input to types

--- app/routes/trajectory.js
+++ app/routes/trajectory.js
@@ -4,7 +4,20 @@ import request from 'ic-ajax';
 
 export default Ember.Route.extend({
   model: function() {
-    return request('/api/rates_pheno');
+    return request('/api/rates_pheno').then(function(result) {
+      var regions = {};
+      var parse_date = d3.time.format("%Y%m%d");
+      result.forEach (function (d) {
+        d.Date = parse_date.parse(String(d.Date));
+        regions[d.Segment] = 1;
+        for (var k in d) {
+          if (k !== "Segment" && k !== "Date") {
+            d[k] = +d[k];
+          }
+        }
+      });
+      return result;
+    });
   }
 });
 
