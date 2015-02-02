d1a04a56d3d8b31a38981eb717afd0afe550af1f
Kemal Eren <kemal@kemaleren.com>
2015-02-02 15:53:58 -0800

    range checking

--- app/components/sequence-range-controls.js
+++ app/components/sequence-range-controls.js
@@ -10,13 +10,42 @@ export default Ember.Component.extend({
   minCoord: 1,
   maxCoord: 1,
 
+  // TODO: this kind of validation and syncing with controller
+  // must be common. Look for more idiomatic way to do it.
+  myRangeStart: 1,
+  myRangeStop: 1,
+
+  fromController: function() {
+    this.set('myRangeStart', +this.get('rangeStart'));
+    this.set('myRangeStop', +this.get('rangeStop'));
+  }.on('didInsertElement'),
+
+  toController: function() {
+    var mystart = +this.get('myRangeStart');
+    var mystop = +this.get('myRangeStop');
+
+    var minCoord = +this.get('minCoord');
+    var maxCoord = +this.get('maxCoord');
+
+    if ((minCoord <= mystart) && (mystart < mystop) && (mystop <= maxCoord)) {
+      this.set('rangeStart', +mystart);
+      this.set('rangeStop', +mystop);
+      this.set('myRangeStart', +mystart);
+      this.set('myRangeStop', +mystop);
+    }
+  },
+
   actions: {
     moveRange: function(offset) {
       // FIXME: why are these sometimes strings???
       var start = +this.get('rangeStart');
       var stop = +this.get('rangeStop');
-      this.set('rangeStart', start + offset);
-      this.set('rangeStop', stop + offset);
+      this.set('myRangeStart', start + offset);
+      this.set('myRangeStop', stop + offset);
+      this.toController();
+    },
+    display: function() {
+      this.toController();
     }
   }
 });
