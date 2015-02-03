f42d6e146c8f3407f7c75e00cd01b2e3eec1ba66
Kemal Eren <kemal@kemaleren.com>
2015-02-03 13:16:59 -0800

    sort mab names

--- app/controllers/neutralization.js
+++ app/controllers/neutralization.js
@@ -7,19 +7,33 @@ function format_percent(frac) {
 }
 
 export default Ember.Controller.extend({
+  mabNames: function() {
+    var json = this.get('model')['neutralization'];
+    var result = [];
+    for (var mab_name in json) {
+      if (json.hasOwnProperty(mab_name)) {
+        result.push(mab_name);
+      }
+    }
+    result.sort();
+    return result;
+  }.property('self.model.neutralization@each'),
+
   mabFeatures: function() {
     var json = this.get('model')['neutralization'];
     var seq_id_to_date = this.get('seqIdToDate');
 
     var mab_table = {};
-    for (var mab_name in json) {
+    var names = this.get('mabNames');
+    for (var i=0; i<names.length; i++) {
+      var mab_name = names[i];
       if (!json.hasOwnProperty(mab_name)) {
         continue;
       }
       var predictions = json[mab_name]['predictions'];
       var table_entry = ensureHas(mab_table, mab_name, {});
-      for (var i=0; i<predictions.length; i++) {
-        var seq = predictions[i];
+      for (var j=0; j<predictions.length; j++) {
+        var seq = predictions[j];
         if (!(seq.id in seq_id_to_date)) {
           // FIXME: what to do with internal nodes?
           continue;
@@ -29,29 +43,34 @@ export default Ember.Controller.extend({
         var elt = ensureHas(table_entry, date, {});
         var new_feats = ensureHas(elt, 'features', []);
         var features = seq.features;
-        for (var j=0; j<features.length; j++) {
-          new_feats.push({name: features[j],
+        for (var k=0; k<features.length; k++) {
+          new_feats.push({name: features[k],
                           value: value > 0});
         }
       }
     }
     return mab_table;
-  }.property('model.neutralization@each', 'seqIdToDate'),
+  }.property('mabNames@each', 'model.neutralization@each', 'seqIdToDate'),
 
-  // TODO: move these to the view?
-  headerNames: function() {
+  sortedDates: function() {
     var d = this.get('model')['dates'];
     var result = [];
     for (var k in d) {
       if(d.hasOwnProperty(k)) {
-        var date = new Date(k);
-        result.push(moment(date).format("MMM YYYY"));
+        result.push(new Date(k));
       }
     }
     result.sort();
-    result.splice(0, 0, 'mab');
     return result;
   }.property('model.dates@each'),
+
+  // TODO: move these to the view?
+  headerNames: function() {
+    var dates = this.get('sortedDates');
+    var result = dates.map(function(d) {return moment(d).format("MMM YYYY")});
+    result.splice(0, 0, 'mab');
+    return result;
+  }.property('sortedDates@each'),
 
   tableData: function() {
     /* [[{value: v,
