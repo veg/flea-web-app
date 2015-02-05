3cb37393a58e5c7922020cc5af99980068bbc725
Kemal Eren <kemal@kemaleren.com>
2015-02-05 12:53:19 -0800

    created rates mock

---
+++ server/mocks/rates.js
@@ -0,0 +1,35 @@
+module.exports = function(app) {
+  var express = require('express');
+  var ratesRouter = express.Router();
+  var fs = require('fs');
+
+  sequencesRouter.get('/', function(req, res) {
+    res.send(JSON.parse(fs.readFileSync(__dirname + '/rates.json', 'ascii')));
+  });
+
+  ratesRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  ratesRouter.get('/:id', function(req, res) {
+    res.send({
+      'rates': {
+        id: req.params.id
+      }
+    });
+  });
+
+  ratesRouter.put('/:id', function(req, res) {
+    res.send({
+      'rates': {
+        id: req.params.id
+      }
+    });
+  });
+
+  ratesRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  app.use('/api/rates', ratesRouter);
+};
