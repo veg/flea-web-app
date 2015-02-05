2b7b2dd0bb274f962e973b98aa982debde2a8270
Kemal Eren <kemal@kemaleren.com>
2015-02-05 12:39:39 -0800

    allow inline style

--- config/environment.js
+++ config/environment.js
@@ -43,5 +43,16 @@ module.exports = function(environment) {
 
   }
 
+  // config/environment.js
+  ENV.contentSecurityPolicy = {
+    'default-src': "'none'",
+    'script-src': "'self'",
+    'font-src': "'self'",
+    'connect-src': "'self'",
+    'img-src': "'self'",
+    'style-src': "'self' 'unsafe-inline'",
+    'media-src': "'self'"
+  }
+
   return ENV;
 };
