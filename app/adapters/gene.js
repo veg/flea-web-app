7ba7831260fd42fccab0596d15c9b42b0d546d37
Kemal Eren <kemal@kemaleren.com>
2015-02-05 13:38:57 -0800

    changed gene model

--- app/adapters/gene.js
+++ app/adapters/gene.js
@@ -3,14 +3,25 @@ import request from 'ic-ajax';
 
 var parse_date = d3.time.format("%Y%m%d");
 
+var GeneInfo = Ember.Object.extend({
+  name: null,
+  data: null,
+});
+
 export default Ember.Object.extend({
   find: function() {
     return request('/api/rates').then(function(result) {
-      var new_result = {};
-      for (var date in result) {
-        if (result.hasOwnProperty(date)) {
-          var d = parse_date.parse(date);
-          new_result[d] = result[date];
+      var new_result = [];
+      for (var name in result) {
+        if (result.hasOwnProperty(name)) {
+          var d = name;
+          if (name !== "Combined") {
+            d = parse_date.parse(name);
+          }
+          new_result.push(GeneInfo.create({
+            name: d,
+            data: result[name]
+          }));
         }
       }
       return new_result;
