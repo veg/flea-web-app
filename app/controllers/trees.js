4cb30c3fb96d81bee4436371d4a40f1d7435cb7c
Kemal Eren <kemal@kemaleren.com>
2015-02-19 13:42:53 -0800

    got phylotree controls working

--- app/controllers/trees.js
+++ app/controllers/trees.js
@@ -3,17 +3,87 @@ import {parse_date, format_date, isString} from '../utils/utils';
 
 export default Ember.Controller.extend({
 
-  tree: function() {
-    var region = 'gp160';
-    var date = 'Combined';
-    var distance = 'Nonsynonymous only';
-    var tree = this.get('model.trees').filter(function(elt) {
-      return ((elt.region === region) &&
-              (elt.date === date) &&
-              (elt.distance === distance));
-    })[0];
-    return tree.tree;
+  _selectedGenomicRegion: '',
+  _selectedTimePoint: '',
+  _selectedDistanceMeasure: '',
+
+  linkeToSelection: true,
+  showCloneNames: true,
+
+  nestedTrees: function() {
+    var trees = this.get('model.trees');
+    var keys = {};
+    for (var i=0; i < trees.length; i++) {
+      var tree = trees[i];
+      if (!(tree.region in keys)) {
+        keys[tree.region] = {};
+      }
+      if (!(tree.date in keys[tree.region])) {
+        keys[tree.region][tree.date] = {};
+      }
+      if (!(tree.distance in keys[tree.region][tree.date])) {
+        keys[tree.region][tree.date][tree.distance] = tree.tree;
+      }
+    }
+    return keys;
   }.property('model.trees.@each'),
+
+  genomicRegions: function() {
+    var trees = this.get('nestedTrees');
+    return Object.keys(trees);
+  }.property('nestedTrees'),
+
+  handleSelection: function(options, hidden, key, value, oldvalue) {
+    var optionvalues = this.get(options);
+    if (value === undefined) {
+      if (!(_.includes(optionvalues, this.get(hidden)))) {
+        this.set(hidden, optionvalues[0]);
+      }
+    } else {
+      if (_.includes(optionvalues, value)) {
+        this.set(hidden, value);
+      } else {
+        this.set(hidden, optionvalues[0]);
+      }
+    }
+    return this.get(hidden);
+  },
+
+  selectedGenomicRegion: function(key, value, oldvalue) {
+    return this.handleSelection('genomicRegions', '_selectedGenomicRegion', key, value, oldvalue);
+  }.property('genomicRegions'),
+
+  selectedTimePoint: function(key, value, oldvalue) {
+    return this.handleSelection('timePoints', '_selectedTimePoint', key, value, oldvalue);
+  }.property('timePoints'),
+
+  selectedDistanceMeasure: function(key, value, oldvalue) {
+    return this.handleSelection('distanceMeasures', '_selectedDistanceMeasure', key, value, oldvalue);
+  }.property('distanceMeasures'),
+
+  timePoints: function() {
+    var trees = this.get('nestedTrees');
+    var region = this.get('selectedGenomicRegion');
+    return Object.keys(trees[region]);
+  }.property('nestedTrees', 'selectedGenomicRegion'),
+
+  distanceMeasures: function() {
+    var trees = this.get('nestedTrees');
+    var region = this.get('selectedGenomicRegion');
+    var date = this.get('selectedTimePoint');
+    return Object.keys(trees[region][date]);
+  }.property('nestedTrees', 'selectedGenomicRegion', 'selectedTimePoint'),
+
+  tree: function() {
+    var region = this.get('selectedGenomicRegion');
+    var date = this.get('selectedTimePoint');
+    var distance = this.get('selectedDistanceMeasure');
+    var tree = this.get('nestedTrees')[region][date][distance];
+    return tree;
+  }.property('nestedTrees',
+             'selectedGenomicRegion',
+             'selectedTimePoints',
+             'selectedDistanceMeasure'),
 
   // FIXME: code duplication. Same function used in neutralization controller.
   // Where to put this to share it?
