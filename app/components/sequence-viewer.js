14b4ebb9557b78cf17edfa760ee736d8e34ba041
Kemal Eren <kemal@kemaleren.com>
2015-02-20 00:55:24 -0800

    show HXB2 coordinates

--- app/components/sequence-viewer.js
+++ app/components/sequence-viewer.js
@@ -11,7 +11,7 @@ export default Ember.Component.extend({
 
   transformCoord: function(coord) {
     var ref = this.get('refCoords');
-    return ref.indexOf(coord);
+    return ref.indexOf(coord) + 1; // convert from 0-index to 1-index
   },
 
   alnStart: function() {
@@ -22,10 +22,44 @@ export default Ember.Component.extend({
     return this.transformCoord(this.get('rangeStop'));
   }.property('refCoords', 'rangeStop'),
 
+  refHTML: function() {
+    var coordinates = ["","",""];
+    var selected_string = "";
+    var last_hs = -5;
+    var ref_map = this.get('refCoords');
+    // TODO: there is surely a more elegent way of building this html
+    for (var s = this.get('alnStart'); s <= this.get('alnStop'); s++) {
+      var hs = ref_map[s - 1];  // convert to 0-index
+      var str = "";
+      if (hs < 10) {
+        str = "  " + hs;
+      } else {
+        if (hs < 100) {
+          str = " " + hs;
+        } else {
+          str = "" + hs;
+        }
+      }
+      // if (show_selected) {
+      //   selected_string += (_positive_selection['combined'].indexOf (s)>=0) ? "+" : "&nbsp;";
+      // }
+      if (last_hs === hs) {
+        str = "INS";
+      }
+      coordinates[0] += "<span class = '_seq_hover_seq' data-coord = '" + s +"'>" + (str[0] === " " ? "&nbsp;" : str[0]) + "</span>";
+      coordinates[1] += "<span class = '_seq_hover_seq' data-coord = '" + s +"'>" + (str[1] === " " ? "&nbsp;" : str[1]) + "</span>";
+      coordinates[2] += "<span class = '_seq_hover_seq' data-coord = '" + s +"'>" + (str[2] === " " ? "&nbsp;" : str[2]) + "</span>";
+      last_hs = hs;
+    }
+    var show_selected = false;
+    return coordinates[0] + "<br/>" + coordinates[1] + "<br/>" + coordinates[2] + (show_selected ? "<br/>" + selected_string : "");
+  }.property('alnStart', 'alnStop', 'refCoords'),
+
   mrcaSlice: function() {
     var start = this.get("alnStart");
     var stop = this.get("alnStop");
-    return this.get('mrca').sequence.slice(start, stop + 1);
+    // convert 1-indexed [start, stop] to 0-indexed [start, stop)
+    return this.get('mrca').sequence.slice(start - 1, stop);
   }.property('alnStart', 'alnStop', 'inputSequence.@each'),
 
   groupedSequences: function() {
@@ -40,7 +74,8 @@ export default Ember.Component.extend({
     var mrca = this.get('mrcaSlice');
     var mask = this.get('maskUnchanged');
     var process = function(s) {
-      var result = s.sequence.slice(start, stop + 1);
+      // convert 1-indexed [start, stop] to 0-indexed [start, stop)
+      var result = s.sequence.slice(start - 1, stop);
       if (mask) {
         result = result.split('').map(function(aa, idx) {
           return aa === mrca[idx] ? '.' : aa;
