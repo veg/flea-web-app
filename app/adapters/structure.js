c500258210f425396199a7954b2bef9513e2b1ad
Kemal Eren <kemal@kemaleren.com>
2015-02-25 01:58:04 -0800

    added assets

--- app/adapters/structure.js
+++ app/adapters/structure.js
@@ -3,7 +3,7 @@ import request from 'ic-ajax';
 
 export default Ember.Object.extend({
   find: function(session_id) {
-    var url = '/api/' + session_id + '/structure';
+    var url = '/assets/env_structure.pdb';
     return request(url).then(function(result) {
       var structure = pv.io.pdb(result);
       return structure;
