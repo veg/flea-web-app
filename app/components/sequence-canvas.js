a2a435995c08e513a9d322a5a5f1f82042fc6787
Kemal Eren <kemal@kemaleren.com>
2015-04-08 16:39:11 -0700

    draw selected positions

--- app/components/sequence-canvas.js
+++ app/components/sequence-canvas.js
@@ -10,14 +10,37 @@ export default Ember.Component.extend({
   alnRanges: null,
   minCoord: 1,
   maxCoord: 1,
+  selectedPositions: null,
   predefinedRegions: null,
   refCoord: null,
 
   width: Ember.computed.alias('maxCoord'),
 
   didInsertElement: function() {
-    var ctx = this.get('element').getContext('2d')
-    ctx.fillStyle = "#000";
+    this.set('ctx', this.get('element').getContext('2d'));
+    this.draw();
+  },
+
+  draw: function() {
+    console.log('drawing');
+    this._empty();
+    var ctx = this.get('ctx');
+    var h = this.get('height');
+
+    ctx.strokeRect(0, 0, this.get('width'), this.get('height'));
+
+    // draw selected positions
+    this.get('selectedPositions').forEach(function(p) {
+      ctx.beginPath();
+      ctx.moveTo(p, 0);
+      ctx.lineTo(p, h);
+      ctx.stroke();
+    });
+  }.observes('maxCoord', 'predefinedRegions', 'refCoord', 'selectedPositions.[]'),
+
+  _empty: function() {
+    var ctx = this.get('ctx');
+    ctx.fillStyle = '#fff';
     ctx.fillRect(0, 0, this.get('width'), this.get('height'));
   }
 });
