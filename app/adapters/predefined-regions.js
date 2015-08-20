604036e98a45ed2c09c49f6f08190d0f3dc3a767
Kemal Eren <kemal@kemaleren.com>
2015-08-20 14:50:53 -0700

    store predefined regions as 1-indexed closed ranges

--- app/adapters/predefined-regions.js
+++ app/adapters/predefined-regions.js
@@ -5,7 +5,14 @@ import request from 'ic-ajax';
 export default Ember.Object.extend({
   find: function() {
     var url = config.baseURL + 'assets/predefined_regions.json';
-    return request(url).then(d => d["regions"]);
+    return request(url).then(function(data) {
+      var regions = data['regions'];
+      // convert from 1-indexed [start, stop] to  0-index [start, stop)
+      for (let k in regions) {
+        regions[k].start -= 1;
+      }
+      return regions;
+    });
   }
 });
 
