5f7d4e4a182bbddb6e65425b3ecfa01b661fcbde
Kemal Eren <kemal@kemaleren.com>
2015-08-22 23:29:34 -0700

    Brocfile.js transition

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,5 +19,50 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {
+    destDir: 'assets'
+  });
+  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', {
+    destDir: 'fonts'
+  });
+  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', {
+    destDir: 'fonts'
+  });
+  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', {
+    destDir: 'fonts'
+  });
+  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
+    destDir: 'fonts'
+  });
+  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2', {
+    destDir: 'fonts'
+  });
+  
+  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+  
+  app.import('bower_components/d3/d3.js');
+  
+  app.import('bower_components/bootstrap-multiselect/dist/css/bootstrap-multiselect.css')
+  app.import('bower_components/bootstrap-multiselect/dist/js/bootstrap-multiselect.js')
+  
+  app.import("bower_components/font-awesome/css/font-awesome.css");
+  app.import("bower_components/font-awesome/fonts/fontawesome-webfont.eot", { destDir: "fonts" });
+  app.import("bower_components/font-awesome/fonts/fontawesome-webfont.svg", { destDir: "fonts" });
+  app.import("bower_components/font-awesome/fonts/fontawesome-webfont.ttf", { destDir: "fonts" });
+  app.import("bower_components/font-awesome/fonts/fontawesome-webfont.woff", { destDir: "fonts" });
+  app.import("bower_components/font-awesome/fonts/fontawesome-webfont.woff2", { destDir: "fonts" });
+  app.import("bower_components/font-awesome/fonts/FontAwesome.otf", { destDir: "fonts" });
+  
+  app.import('bower_components/lodash/lodash.js');
+  
+  app.import('bower_components/moment/moment.js');
+  
+  app.import('bower_components/phylotree/phylotree.js');
+  app.import('bower_components/phylotree/phylotree.css');
+  
+  app.import('bower_components/bio-pv-1.5.0/js/bio-pv.min.js');
+  
+  
   return app.toTree();
 };
