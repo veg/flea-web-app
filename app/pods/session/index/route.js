53cf4b86eba40e2307e366655aac3ed4ac47f19d
Kemal Eren <kemal@kemaleren.com>
2015-09-02 20:04:06 -0700

    transition to sequences tab by default, since trajectories may not exist

--- app/pods/session/index/route.js
+++ app/pods/session/index/route.js
@@ -2,11 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   redirect: function() {
-    var hyphy = this.get('model.runinfo.configuration.Tasks.hyphy_analysis');
-    if (hyphy) {
-      this.transitionTo('session.trajectory');
-    } else {
-      this.transitionTo('session.gene');
-    }
+    this.transitionTo('session.sequences');
   }
 });
