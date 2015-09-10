9bde5a067d94628912d6cc3bf8d7be89b83d71b2
Kemal Eren <kemal@kemaleren.com>
2015-09-10 10:39:00 -0700

    support either string or bool for hyphy_analysis config parameter

--- app/pods/session/controller.js
+++ app/pods/session/controller.js
@@ -3,7 +3,15 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   showTrajectory: function() {
     try {
-      return this.get('model.runinfo.configuration.Tasks.hyphy_analysis') === "True";
+      var val = this.get('model.runinfo.configuration.Tasks.hyphy_analysis');
+      // used to be a string, but now it is a JSON bool. Have to check both.
+      if (val === "True") {
+        return true;
+      }
+      if (val === "False") {
+        return false;
+      }
+      return val;
     } catch (err) {
       return false;
     }
