d33bdad1270e9d5312624f61c5ef14bfb2a1f605
Kemal Eren <kemal@kemaleren.com>
2017-08-31 09:44:26 -0400

    always show trajectory

--- app/pods/session/controller.js
+++ app/pods/session/controller.js
@@ -1,19 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  showTrajectory: function() {
-    try {
-      var val = this.get('model.runinfo.configuration.Tasks.evo_history');
-      // used to be a string, but now it is a JSON bool. Have to check both.
-      if (val === "True") {
-        return true;
-      }
-      if (val === "False") {
-        return false;
-      }
-      return val;
-    } catch (err) {
-      return false;
-    }
-  }.property('model.runinfo.configuration.Tasks.hyphy_analysis_slow')
+
 });
--- app/pods/session/trajectory/route.js
+++ app/pods/session/trajectory/route.js
@@ -2,16 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    let show_trajectory = this.controllerFor('session').get('showTrajectory');
-    if (show_trajectory) {
-      let session_id = this.modelFor('session').session_id;
-      return Ember.RSVP.hash({
-	dates: this.store.find('dates', session_id),
-	trajectory: this.store.find('trajectory', session_id),
-	disabled: false,
-      });
-    } else {
-      return {disabled: true};
-    }
+    let session_id = this.modelFor('session').session_id;
+    return Ember.RSVP.hash({
+      dates: this.store.find('dates', session_id),
+      trajectory: this.store.find('trajectory', session_id),
+      disabled: false,
+    });
   }
 });
