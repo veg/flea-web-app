49b5774b62d5b82bc3fce74e845c7a5ec91a358b
Kemal Eren <kemal@kemaleren.com>
2017-10-21 12:21:20 -0700

    remove runinfo. use fluid container.

--- app/pods/session/route.js
+++ app/pods/session/route.js
@@ -4,7 +4,6 @@ export default Ember.Route.extend({
   model: function(params) {
     return Ember.RSVP.hash({
       session_id: params.session_id,
-      runinfo: this.store.find('runinfo', params.session_id)
     });
   }
 });
