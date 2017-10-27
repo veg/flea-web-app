2f1a17e7611938e3f8c0254639f5185b05621598
Kemal Eren <kemal@kemaleren.com>
2017-10-26 17:45:03 -0700

    request predefined_regions from /api/results

--- app/adapters/predefined-regions.js
+++ app/adapters/predefined-regions.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default Ember.Object.extend({
   ajax: Ember.inject.service(),
 
-  find: function() {
-    var url = '/assets/predefined_regions.json';
+  find: function(session_id) {
+    var url = config.apiURL + 'sessions/' + session_id + '/predefined_regions';
     return this.get("ajax").request(url).then(function(data) {
       var regions = data['regions'];
       // convert from 1-indexed [start, stop] to  0-index [start, stop)
