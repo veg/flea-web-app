07ca3ec5cb76da4c14f5bde6adfff8a9ff8eb3ea
Kemal Eren <kemal@kemaleren.com>
2017-11-10 10:41:16 -0800

    add button for downloading zip file

--- app/pods/session/controller.js
+++ app/pods/session/controller.js
@@ -1,8 +1,14 @@
 import Ember from 'ember';
 import config from '../../config/environment';
+import { computed } from 'ember-decorators/object';
 
 export default Ember.Controller.extend({
 
-  fleaMode: config['fleaMode']
+  fleaMode: config['fleaMode'],
+
+  @computed('model.session_id')
+  downloadURL(sessionId) {
+    return config.apiURL + 'zips/' + sessionId + ".zip";
+  },
 
 });
