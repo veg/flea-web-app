dc2f1ef6f0d908109fdcf785137886b59cd9f404
Kemal Eren <kemal@kemaleren.com>
2017-12-21 18:02:26 -0800

    transition to protein tab in db mode

--- app/pods/session/index/route.js
+++ app/pods/session/index/route.js
@@ -1,7 +1,14 @@
 import Ember from 'ember';
+import config from '../../../config/environment';
 
 export default Ember.Route.extend({
+  fleaMode: config['fleaMode'],
+
   redirect() {
-    this.transitionTo('session.mds');
+    if (this.get('fleaMode')) {
+      this.transitionTo('session.mds');
+    } else {
+      this.transitionTo('session.protein');
+    }
   }
 });
